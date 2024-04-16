3eb5a50309807eea056b6ff4f8008202ed4acad9
smcloud65 <28224441+smcloud65@users.noreply.github.com>
2024-04-16 17:54:06 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-12345"  # Ensure the bucket name is globally unique
+  acl    = "public"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
 }
